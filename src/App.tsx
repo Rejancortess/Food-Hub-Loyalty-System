9d0cae0cb84f528c270401689d19a3dc8e1cf3d1
JanYuro <rejanmamugay35@gmail.com>
2026-02-20 22:26:42 +0800

    Set-Up Project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen bg-red-500 flex items-center justify-center text-5xl font-extrabold">
+      Test
+    </div>
+  );
+};
+
+export default App;
